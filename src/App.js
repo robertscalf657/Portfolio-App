58f8e8a9fa47bb0117e130b457e4a5ad3b4f3bee
Robert Scalf <robertscalf657@gmail.com>
2022-04-18 14:36:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Profile from './PortfolioContainerComponents/Home/Profile';
 import './App.css';
-import Footer from './PortfolioContainerComponents/Home/Footer/Footer';
+import Footer from './PortfolioContainerComponents/Home/Footer/Footer'
 
 
 function App() {
   return (
     <div className="App">
       <Profile/>
-      <Footer/>
+      
     </div>
   );
 }
