8968bdf028d1e62379d6c8bd60f213e6d25533ea
Robert Scalf <robertscalf657@gmail.com>
2022-04-18 14:05:58 -0500

    first commit

--- src/PortfolioContainerComponents/Home/Footer/Footer.js
+++ src/PortfolioContainerComponents/Home/Footer/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <div className='footer-container'>
       <div className='footer-parent'>
-          <img src={require ('../../../assets/home/shape-bg.png')} alt ='no internet connection'/>
+          <img src={require ('../../../assets/home/shape-bg.png').default} alt ='no internet connection'/>
       </div>
     </div>
   )
